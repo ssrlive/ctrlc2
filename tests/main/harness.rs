8864f2cde1d411697d852aef8c3f503a075db84c
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-12-14 18:32:34 +0800

    Update harness.rs

--- tests/main/harness.rs
+++ tests/main/harness.rs
@@ -48,7 +48,7 @@ pub mod platform {
         Cached(Vec<u8>),
     }
 
-    static mut OLD_OUT: *mut Output = 0 as *mut Output;
+    static mut OLD_OUT: *mut Output = std::ptr::null_mut::<Output>();
 
     impl io::Write for Output {
         fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
